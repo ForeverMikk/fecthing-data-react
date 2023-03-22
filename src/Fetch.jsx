25ccfea1602151895e94e6968dec4e944a8df06a
unknown <michaelmolina679@gmail.com>
2023-03-21 18:52:56 -0600

    adding abort controller

--- src/Fetch.jsx
+++ src/Fetch.jsx
@@ -1,12 +1,13 @@
 import { useFecth } from "./services/useFetch";
 
 function Fecth() {
-    const  { data, loading, error }  = useFecth('https://jsonplaceholder.typicode.com/users');    
-    console.log(data);
+    const  { data, loading, error, handleCancelRequest }  = useFecth('https://jsonplaceholder.typicode.com/users');    
+    // console.log(data);
     
     return (
         <div>
             <div className="card">
+                <button onClick={handleCancelRequest}>Cancel Request</button>
                 {error && <h2>Error: {error}</h2>}
                 {loading && <h2>Loaing...</h2>}
                 <ul>
--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -8,23 +8,44 @@ export function useFecth(url) {
     const [loading, setLoading] = useState(true);
     // Un error se guardara en este estado
     const [error, setError] = useState(null);
+    // Aqui se guarda el controller
+    const [controller, setController] = useState(null);
     
 
     useEffect(() => {
+        //abort controller cancela la ejecucion de la peticion en caso de que desmonte el componente
+        //o que no se renderize
+        const abortController = new AbortController();
+        setController(abortController);
         setLoading(true);
 
-        fetch(url)
+        fetch(url, { signal: abortController.signal })
             .then((response) => response.json())
             // Se setea la data en el estado
             .then((data) => setData(data))
             // Se setea el error (en caso de haber uno)
-            .catch((error) => setError(error))
+            .catch((error) => {
+                if(error.name === "AbortError"){
+                    console.log("Request Cancelled");
+                } else{
+                    setError(error);
+                }
+            })
             // Una vez terminan las ejecuciones setea false el loading
             .finally(() => setLoading(false));
+        return () => abortController.abort();
         }, []);
+
+        //En caso de que quieras cancelar la Request manualmente usa esto
+        const handleCancelRequest = () => {
+            if(controller){
+                controller.abort();
+                setError("Request Cancelled");
+            }
+        }
     
         // Regresas toda la data a tu componente
-        return { data, loading, error }
+        return { data, loading, error, handleCancelRequest }
 }
 
 
